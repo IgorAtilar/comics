be48f44a54e84fd8ce37913c73cd93c0acf6e0b4
Igor Atilar <igoratilar10@gmail.com>
2023-09-03 12:39:16 -0300

    feat: add theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				action: "#737373",
+				adventure: "#F4F4F4",
+				comedy: "#F1F1F1",
+				drama: "#FFFFFF",
+				fantasy: "#262626",
+			}
+		},
 	},
 	plugins: [],
 }
