8cb0f29d43e6dd22924f264f486a5c0dc63c370a
Igor Atilar <igoratilar10@gmail.com>
2023-09-03 16:47:41 -0300

    chore: add font config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				action: "#737373",
-				adventure: "#F4F4F4",
-				comedy: "#F1F1F1",
-				drama: "#FFFFFF",
-				fantasy: "#262626",
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        action: '#737373',
+        adventure: '#F4F4F4',
+        comedy: '#F1F1F1',
+        drama: '#FFFFFF',
+        fantasy: '#262626'
+      },
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+        heading: ['Koulen', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+};
