956a625e996db32c717e9288da331a4c9090b63c
Igor Atilar <igoratilar10@gmail.com>
2023-09-04 22:08:28 -0300

    chore: add env example

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly MARVEL_API_KEY: string;
+  readonly MARVEL_API_HASH: string;
+  readonly PUBLIC_MARVEL_API: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
