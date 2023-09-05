b00fee0f064b9c4b929ed95d5deee02bfa1f46e9
Igor Atilar <igoratilar10@gmail.com>
2023-09-05 12:05:52 -0300

    chore: change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   integrations: [react(), tailwind()],
   adapter: vercel()
 });
