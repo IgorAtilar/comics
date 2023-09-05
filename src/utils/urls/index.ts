59197055e9e1cb1b7205397ef5970535bced0f4e
Igor Atilar <igoratilar10@gmail.com>
2023-09-05 12:25:59 -0300

    chore: change getSearchUrl

--- src/utils/urls/index.ts
+++ src/utils/urls/index.ts
@@ -1,10 +1,10 @@
 export const getSearchUrl = ({
   query,
-  page
+  page = 1
 }: {
   query: string;
   page?: number;
-}) => `/search?q=${query}${page ? `&page=${page}` : ''}`;
+}) => `/search?q=${query}&page=${page}`;
 
 export const getComicUrl = ({ id }: { id: string }) => `/comic?id=${id}`;
 
