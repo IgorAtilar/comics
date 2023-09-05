3625404fdd85f35beee641455c325f0fa1cfa21a
Igor Atilar <igoratilar10@gmail.com>
2023-09-05 03:54:45 -0300

    fix: change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [react(), tailwind()],
-  adapter: vercel({
-    analytics: true,
-  })
+  adapter: vercel()
 });
